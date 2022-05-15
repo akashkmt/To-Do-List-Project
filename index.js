ed7439753e889e0dca220e31a02391554f773bac
Akash Kumawat <97526754+akashkmt@users.noreply.github.com>
2022-05-15 12:24:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,15 +30,18 @@ document.querySelector("#input").addEventListener("keydown", (event) => {
     checkIcon.className = "fas fa-check-square";
     checkIcon.style.color = "lightgray";
     checkIcon.addEventListener("click", () => {
-      checkIcon.style.color = "limegreen";
+      if(checkIcon.style.color == "limegreen"){
+        checkIcon.style.color = "lightgray";
+      }
+      else{
+        checkIcon.style.color = "limegreen";
+      }
     })
     div.appendChild(checkIcon);
   
     trashIcon.className = "fas fa-trash";
     trashIcon.style.color = "darkgray";
-    // trashIcon.addEventListener("click", () => {
-    //   item.remove();
-    // })
+    
     trashIcon.addEventListener('click', () => {
         allTasks.splice(index, 1);
         localStorage.setItem('allTasks',JSON.stringify(allTasks));
